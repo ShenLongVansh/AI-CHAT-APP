004782d1fe147f25d351ccad95aaec8128f59964
Vansh Sharma <shenlong.codes@gmail.com>
2025-05-12 01:15:26 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "react-chatbot-app-9e242.firebaseapp.com",
-  projectId: "react-chatbot-app-9e242",
-  storageBucket: "react-chatbot-app-9e242.appspot.com",
-  messagingSenderId: "753331285531",
-  appId: "1:753331285531:web:4be6567ea858b10698d2be",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
