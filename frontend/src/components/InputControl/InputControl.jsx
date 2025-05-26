e7375240f1690c80e899491a29c25ac7d6547ab2
Vansh Sharma <shenlong.codes@gmail.com>
2025-05-26 10:50:43 +0530

    Update InputControl.jsx

--- frontend/src/components/InputControl/InputControl.jsx
+++ frontend/src/components/InputControl/InputControl.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import styles from "./InputControl.module.css";
+import hidePng from "../../assets/hide.png"
+import showPng from "../../assets/show.png"
 
 function InputControl({ label, type = "text", ...props }) {
   const [showPassword, setShowPassword] = useState(false);
 
   const isPasswordType = type === "password";
+  const showImage = ""
 
   return (
     <div className={styles.container} style={{ position: "relative" }}>
@@ -26,7 +29,9 @@ function InputControl({ label, type = "text", ...props }) {
             color: "#555"
           }}
         >
-          {showPassword ? "Hide" : "Show"}
+          <img src={showPassword ? hidePng : showPng} width={30} height={20}  alt={showPassword ? "Hide" : "Show"}> 
+          
+          </img>
         </span>
       )}
     </div>
